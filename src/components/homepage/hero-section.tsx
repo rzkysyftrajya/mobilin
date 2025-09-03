e60bf5fc406f59be0790491b99392d571128cbdb
rzkysyftra <riskisyafutrajaya@gmail.com>
2025-09-03 11:54:15 +0700

    Update hero-section.tsx

--- src/components/homepage/hero-section.tsx
+++ src/components/homepage/hero-section.tsx
@@ -30,7 +30,7 @@ export default function HeroSection({
   return (
     <section className="relative h-[80vh] min-h-[500px] flex items-center text-white overflow-hidden">
       <Image
-        src="/hero-section.webp"
+        src="/hero-section.png"
         alt="Jalanan perkotaan yang sibuk dengan mobil"
         data-ai-hint="scenic road car"
         fill
