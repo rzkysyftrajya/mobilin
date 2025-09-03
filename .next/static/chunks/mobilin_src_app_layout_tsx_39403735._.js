b51d2af36431b55e8753579ed5bc5b62418730b8
Roby047 <robyjaya047@gmail.com>
2025-09-03 11:13:55 +0700

    Add node_modules and other generated files to .gitignore

--- .next/static/chunks/mobilin_src_app_layout_tsx_39403735._.js
+++ .next/static/chunks/mobilin_src_app_layout_tsx_39403735._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__d07968ef._.css",
-  "static/chunks/e8b9d_93f4185a._.js",
+  "static/chunks/e8b9d_4704d29d._.js",
   "static/chunks/mobilin_src_8cd95a20._.js"
 ],
     source: "dynamic"
